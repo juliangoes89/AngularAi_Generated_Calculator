c7f003a0de950be0e047ce48dc4c8ea40bd2e702
Julian Gomez Espinal <juliangoes89@hotmail.com>
2026-02-20 13:37:33 -0500

    arreglar problema de lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
